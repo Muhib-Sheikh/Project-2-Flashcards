109ff275e9741140c7c191588dae2c769a8f2d95
Muhib Sheikh <muhib7005@gmail.com>
2023-03-07 01:55:47 -0500

    added project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Card from './components/Card';
+
+const App = () => {
+
+  return (
+    <div className="App">
+    <h1>Arabic for Beginners!!</h1>
+    <h2>Visiting an Arabic speaking country? Wanting to learn some basics? This is the perfect place to learn some common words/phrases!</h2>
+    <h4>Number of cards: 10</h4>
+    <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { useState } from "react";
+import "./Card.css";
+
+const Card = () => {
+
+  const [cards, setCards] = useState(0);
+
+  const cardPairs = [
+    {
+      title: "As-salaam alaikum لسلام عليكم",
+      definition: "Peace be upon you"
+    },
+    {
+      title: "Sabah al-khair صباح الخير",
+      definition: "Good morning"
+    },
+    {
+      title: "Shukran ﺷﻜﺮﺍﹰ",
+      definition: "Thank you"
+    },
+    {
+      title: "Lazeeza لذيذ",
+      definition: "Delicious"
+    },
+    {
+      title: "Najma نجوم",
+      definition: "Stars"
+    },
+    {
+      title: "Jameela جميل",
+      definition: "Beautiful"
+    },
+    {
+      title: "Ma'an مـَييـَه",
+      definition: "Water"
+    },
+    {
+      title: "Habibi حبيبي",
+      definition: "My love"
+    },
+    {
+      title: "Insha Allah إن شاء الله",
+      definition: "God willing"
+    },
+    {
+      title: "Yalla يللا",
+      definition: "Let's go"
+    },
+  ];
+  
+  const handleClick = () => {
+    setCards(Math.floor(Math.random() * cardPairs.length))
+  }
+  
+
+  return (
+    <div>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <h1>{cardPairs[cards].title}</h1>
+          </div>
+          <div className="flip-card-back">
+            <h1>{cardPairs[cards].definition}</h1>
+          </div>
+        </div>
+      </div>
+      <div>
+        <button onClick={handleClick}>Next</button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
