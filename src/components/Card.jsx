346866c4decb7c92abff48cc684b942940dc07a3
Muhib Sheikh <muhib7005@gmail.com>
2023-03-14 02:40:03 -0400

    updated code for part2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,60 +3,114 @@ import { useState } from "react";
 import "./Card.css";
 
 const Card = () => {
+  let isCorrect = false;
+  let isIncorrect = false;
 
   const [cards, setCards] = useState(0);
+  const [form, setForm] = useState({ guess: "" });
 
-  const cardPairs = [
+  const [style, setStyle] = useState("none");
+
+  const [cardPairs, setCardPairs] = useState([
     {
       title: "Welcome!",
-      definition: "Click next to get started"
+      definition: "Click next to get started",
     },
     {
       title: "As-salaam alaikum لسلام عليكم",
-      definition: "Peace be upon you"
+      definition: "Peace be upon you",
     },
     {
       title: "Sabah al-khair صباح الخير",
-      definition: "Good morning"
+      definition: "Good morning",
     },
     {
       title: "Shukran ﺷﻜﺮﺍﹰ",
-      definition: "Thank you"
+      definition: "Thank you",
     },
     {
       title: "Lazeeza لذيذ",
-      definition: "Delicious"
+      definition: "Delicious",
     },
     {
       title: "Najma نجوم",
-      definition: "Stars"
+      definition: "Stars",
     },
     {
       title: "Jameela جميل",
-      definition: "Beautiful"
+      definition: "Beautiful",
     },
     {
       title: "Ma'an مـَييـَه",
-      definition: "Water"
+      definition: "Water",
     },
     {
       title: "Habibi حبيبي",
-      definition: "My love"
+      definition: "My love",
     },
     {
       title: "Insha Allah إن شاء الله",
-      definition: "God willing"
+      definition: "God willing",
     },
     {
       title: "Yalla يللا",
-      definition: "Let's go"
+      definition: "Let's go",
     },
-  ];
-  
-  const handleClick = () => {
-    setCards(Math.floor((Math.random() * cardPairs.length - 1) + 1))
-  }
-  
+  ]);
+
+  const showNextCard = () => {
+    // console.log(cards)
+    if (cards + 1 !== cardPairs.length) {
+      setCards(cards + 1);
+      setStyle("none");
+      setForm({ 'guess': "" });
+    }
+  };
+
+  const showPrevCard = () => {
+    // console.log(cards)
+    if (cards !== 0) {
+      setCards(cards - 1);
+      setStyle("none");
+      setForm({ 'guess': "" });
+    }
+  };
+
+  const handleShuffle = () => {
+    setCardPairs(
+      cardPairs
+        .slice(0, 1)
+        .concat(
+          cardPairs
+            .slice(1, cardPairs.length)
+            .sort((a, b) => 0.5 - Math.random())
+        )
+    );
+    // console.log(cardPairs);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    submitForm(form);
+  };
+
+  const handleChange = (e) => {
+    const newForm = {};
+    const newValue = e.target.value;
+    const key = e.target.name;
+    newForm[key] = newValue;
+    setForm({ ...newForm });
+  };
+
+  const submitForm = () => {
+    // console.log(form.guess);
+    // console.log(cards);
+    if (form.guess === cardPairs[cards].definition) {
+      setStyle("correct");
+    } else {
+      setStyle("incorrect");
+    }
+  };
 
   return (
     <div>
@@ -71,7 +125,23 @@ const Card = () => {
         </div>
       </div>
       <div>
-        <button onClick={handleClick}>Next</button>
+        <form onSubmit={handleOnSubmit}>
+          <h3>Make a guess: </h3>
+          <input
+            value={form.guess}
+            type="text"
+            onChange={handleChange}
+            name="guess"
+            placeholder="Write your guess here"
+            className={style}
+          />
+          <input type="submit" value="Submit" className="action-button" />
+        </form>
+      </div>
+      <div>
+        <button onClick={showNextCard}>Next</button>
+        <button onClick={showPrevCard}>Prev</button>
+        <button onClick={handleShuffle}>Shuffe</button>
       </div>
     </div>
   );
